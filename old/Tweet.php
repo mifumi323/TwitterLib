b0e90e70224b1a7f1229b15099c8940745fa49df
mifumi323 <mifumi323@gmail.com>
2024-06-01 23:32:57 +0900

    古い名前空間でも使えるように

---
+++ old/Tweet.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MifuminLib\TwitterLib;
+
+/**
+ * @deprecated 2.0.0 use \Mifumi323\TwitterLib\Tweet
+ */
+class Tweet extends \Mifumi323\TwitterLib\Tweet
+{
+}
---
+++ old/Twitter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MifuminLib\TwitterLib;
+
+/**
+ * @deprecated 2.0.0 use \Mifumi323\TwitterLib\Twitter
+ */
+class Twitter extends \Mifumi323\TwitterLib\Twitter
+{
+}
